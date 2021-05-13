c3e7b4918a3a4f35a014bd86c6b7a23e8e977987
unknown <camiaraquecaro17@gmail.com>
2021-05-13 12:46:13 -0500

    [ADD]Services Api and Alerts Toast

---
+++ frontend/src/app/services/alerts/alerts.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { ToastrService } from "ngx-toastr";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertsService {
+
+  constructor(private toast: ToastrService) { }
+
+  showSuccess(text, title) {
+    this.toast.success(text, title);
+  }
+
+  showError(text, title) {
+    this.toast.error(text, title);
+  }
+
+  showWarning(text, title) {
+    this.toast.info(text, title);
+  }
+}
--- frontend/src/app/services/api/api.service.ts
+++ frontend/src/app/services/api/api.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders} from "@angular/common/http";
+import { Observable, Subject } from "rxjs";
 // Models Interfaces
 import {ILogin} from "../../models/login.interface";
 import { IRegister } from "../../models/register.interface";
 import {IResponse} from "../../models/response.interface";
-
+import { IProducts } from "../../models/products.interface";
+import { ICategory } from 'src/app/models/category.interface';
+import { tap } from 'rxjs/operators';
+import { IProfile } from 'src/app/models/profile.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +17,20 @@ export class ApiService {
 
   url:string = "http://localhost:3000/";
 
+  private _refresh$ = new Subject<void>();
+
+  headers = new HttpHeaders({
+    "Content-Type": "application/json",
+    "auth-token": localStorage.getItem("auth-token")
+  })
+
   status:number;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  get resfresh$() {
+    return this._refresh$;
+  }
 
   login(form: ILogin): Observable<IResponse> {
     let url_api_signin = this.url + "api/signin";
@@ -26,5 +40,80 @@ export class ApiService {
   register(form: IRegister): Observable<IResponse> {
     let url_api_signup = this.url + "api/signup";
     return this.http.post<IResponse>(url_api_signup, form);
+  }
+
+  isLogged(): boolean {
+    return !!localStorage.getItem("auth-token");
+  }
+
+  getProducts(): Observable<IProducts[]> {
+    let url_api_getProducts = this.url + "api/products";
+    return this.http.get<IProducts[]>(url_api_getProducts);
+  }
+
+  getProduct(id): Observable<IProducts> {
+    let url_api_getProduct = this.url + "api/products/" + id;
+    return this.http.get<IProducts>(url_api_getProduct);
+  }
+
+  getCategorys(): Observable<ICategory[]> {
+    let url_api_getCategorys = this.url + "api/category";
+    return this.http.get<ICategory[]>(url_api_getCategorys);
+  }
+
+  postProduct(form:IProducts): Observable<IProducts> {
+    let url_api_createProducts = this.url + "api/products";
+    return this.http.post<IProducts>(url_api_createProducts, form);
+  }
+
+  putProduct(form:IProducts): Observable<IProducts> {
+    let url_api_updateProduct = this.url + "api/products/" + form.id
+    return this.http.put<IProducts>(url_api_updateProduct, form);
+  }
+
+  deleteProduct(form:IProducts): Observable<IProducts> {
+    let url_api_deleteProduct = this.url + "api/products/" + form.id
+    return this.http.delete<IProducts>(url_api_deleteProduct);
+  }
+
+  postCategory(form:ICategory): Observable<ICategory> {
+    let url_api_createCategory = this.url + "api/category";
+    return this.http.post<ICategory>(url_api_createCategory, form)
+      .pipe(
+        tap(() => {
+          this._refresh$.next();
+        })
+      )
+  }
+
+  getProductsForCategory(id: number): Observable<ICategory> {
+    let url_api_getProductsForCategory = this.url + "api/category/" + id;
+    return this.http.get<ICategory>(url_api_getProductsForCategory);
+  }
+
+  getProfile(id: string): Observable<IProfile> {
+    let url_api_getProfile = this.url + "api/profile/" + id;
+    return this.http.get<IProfile>(url_api_getProfile, {headers: this.headers});
+  }
+
+  putProfile(form:IProfile): Observable<IProfile> {
+    let url_api_putProfile = this.url + "api/profile/" + localStorage.getItem("id");
+    const newProfile = {
+      "firstName": form.firstName,
+      "lastName": form.lastName,
+      "email": form.email,
+      "profile": {
+        "gender": form.gender,
+        "birthday": form.birthday,
+        "photo": form.photo,
+        "description": form.description
+      }
+    }
+    return this.http.put<IProfile>(url_api_putProfile, newProfile);
+  }
+
+  deleteUser(id): Observable<IProfile> {
+    let url_api_deleteUser = this.url + "api/users/" + id;
+    return this.http.delete<IProfile>(url_api_deleteUser);
   }
 }
