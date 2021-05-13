6cf835a88b359356cb5be80a786b73d9f99de0db
unknown <camiaraquecaro17@gmail.com>
2021-05-13 12:47:44 -0500

    [ADD]App Module and App Routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {ReactiveFormsModule, FormsModule} from "@angular/forms"
 import {HttpClientModule} from "@angular/common/http"
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
 // Routing
 import { AppRoutingModule } from './app.routing.module';
 
@@ -14,6 +17,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { routingComponents } from './app.routing.module';
+import {MatSelectModule} from '@angular/material/select';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
   declarations: [
@@ -28,9 +33,19 @@ import { routingComponents } from './app.routing.module';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    MatSelectModule,
+    MatNativeDateModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [{
+    provide:
+    MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {
+      appearance: "fill"
+    }
+  }, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/app.routing.module.ts
+++ frontend/src/app/app.routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { CreateCategoryComponent } from "./views/create-category/create-category.component";
+import { ProductsCategoryComponent } from "./views/products-category/products-category.component";
 import { CreateProductComponent } from "./views/create-product/create-product.component";
 import { EditProductComponent } from "./views/edit-product/edit-product.component";
 import { HomeComponent } from "./views/home/home.component";
@@ -10,21 +10,25 @@ import {LoginComponent} from "./views/login/login.component";
 import { ProfileComponent } from "./views/profile/profile.component";
 import { RegisterComponent } from "./views/register/register.component";
 
+// Routes Guards
+import { AuthGuard } from "./auth.guard";
+
+
 const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: "full"},
     {path: "login", component: LoginComponent},
     {path: "register", component: RegisterComponent},
-    {path: "home", component: HomeComponent},
-    {path: "profile", component: ProfileComponent},
-    {path: "create-category", component: CreateCategoryComponent},
-    {path: "create-product", component: CreateProductComponent},
-    {path: "edit-product", component: EditProductComponent},
+    {path: "home", component: HomeComponent, canActivate: [AuthGuard]},
+    {path: "profile", component: ProfileComponent, canActivate: [AuthGuard]},
+    {path: "products-category/:id", component: ProductsCategoryComponent, canActivate: [AuthGuard]},
+    {path: "create-product", component: CreateProductComponent, canActivate: [AuthGuard]},
+    {path: "edit-product/:id", component: EditProductComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: "reload"})],
     exports: [RouterModule]
 })
 
 export class AppRoutingModule {}
-export const routingComponents = [LoginComponent, RegisterComponent, HomeComponent, ProfileComponent, CreateCategoryComponent, CreateProductComponent, EditProductComponent]
+export const routingComponents = [LoginComponent, RegisterComponent, HomeComponent, ProfileComponent, ProductsCategoryComponent, CreateProductComponent, EditProductComponent]
